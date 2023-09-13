d2a57f17847577c31e41ae10b86637f700c4275a
Lolisdu <lolidu@yahoo.com.br>
2023-09-13 17:30:51 -0300

    Projeto Angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { TitleComponent } from './components/title/title.component';
+import { CardBigComponent } from './components/card-big/card-big.component';
+import { CardSmallComponent } from './components/card-small/card-small.component';
+import { HomeComponent } from './pages/home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    TitleComponent,
+    CardBigComponent,
+    CardSmallComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
